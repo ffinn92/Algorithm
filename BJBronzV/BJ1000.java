7711e87c48f41a7d573b8c88431ffc2f73b57778
vans <sch12395@gmail.com>
2021-12-04 17:11:44 +0900

    BJ1000번 문제 해결완료

--- BJBronzV/BJ1000.java
+++ BJBronzV/BJ1000.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class BJ1000 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(a+b);
     }
 }
