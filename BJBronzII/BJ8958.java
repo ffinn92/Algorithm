dc49979c0df179b58442a56dfd191351ea6edce1
vans <spentmny@nate.com>
2021-12-09 23:58:12 +0900

    Update input step

--- BJBronzII/BJ8958.java
+++ BJBronzII/BJ8958.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class BJ8958 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
         
+
     }
 }
