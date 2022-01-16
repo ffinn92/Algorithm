ebe792861629fddea80eff190333acc9dae836f4
vans <spentmny@nate.com>
2022-01-16 23:53:01 +0900

    BJ2231번 문제 해결 완료(시도횟수 3회, 검색 사용 O)

--- BJBronzeII/BJ2231.java
+++ BJBronzeII/BJ2231.java
@@ -4,9 +4,25 @@ public class BJ2231 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        int n = sc.nextInt();
+        int result = 0;
 
+        for (int i = 1; i < n; i++) {
+            int number = i;
+            int sum = 0;
 
+            while(number != 0){
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum + i == n){
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
 
     }
 }
